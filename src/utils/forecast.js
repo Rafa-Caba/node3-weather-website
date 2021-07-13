80c7168450ad0d968c8573b1ccb1aa356fcd1d62
Rafae <rafaelcabanillas12@gmail.com>
2021-07-12 22:48:16 -0500

    Updated forcast with -humidity info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=151dd466f94d187ff8ca4e2cc1ed79b1&query=' + latitude + ',' + longitude + '&units=m'
 
     request({ url, json: true }, (error, { body }) => {
-        const location = body.current.weather_descriptions[0] + '. It is current ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.'
+        const location = body.current.weather_descriptions[0] + '. It is current ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. \n' + 'Humidity is: ' + body.current.humidity
 
         if (error) {
             callback('Unable to connect to Weather service!', undefined)
